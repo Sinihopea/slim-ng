f9855a6886e4c7270f298e54a6e90ec7b287b326
Antti Aspinen <antti.aspinen@outlook.com>
2025-06-06 03:15:03 +0300

    Edited comments

--- app/src/slimlock.cpp
+++ app/src/slimlock.cpp
@@ -43,17 +43,22 @@ std::string findValidRandomTheme(const std::string &set);
 void HandleSignal(int sig);
 void *RaiseWindow(void *data);
 
-// I really didn't wanna put these globals here, but it's the only way...
+/* I really didn't wanna put these globals here, but it's the only way... */
 Display *dpy;
 int scr;
-Window win, root;
+Window win;
+Window root;
 Cfg m_config_slimlock;
 Panel *loginPanel;
 std::string themeName = "";
 pam_handle_t *pam_handle;
 struct pam_conv conv = {ConvCallback, nullptr};
-CARD16 dpms_standby, dpms_suspend, dpms_off, dpms_level;
-BOOL dpms_state, using_dpms;
+CARD16 dpms_standby;
+CARD16 dpms_suspend;
+CARD16 dpms_off;
+CARD16 dpms_level;
+BOOL dpms_state;
+BOOL using_dpms;
 int term;
 
 static void die(const char *errstr, ...)
@@ -78,7 +83,7 @@ int main(int argc, char **argv)
 
 	void (*prev_fn)(int);
 
-	// restore DPMS settings should slimlock be killed in the line of duty
+	/* restore DPMS settings should slimlock be killed in the line of duty */
 	prev_fn = signal(SIGTERM, HandleSignal);
 
 	if (prev_fn == SIG_IGN)
@@ -86,13 +91,12 @@ int main(int argc, char **argv)
 		signal(SIGTERM, SIG_IGN);
 	}
 
-	// create a lock file to solve mutliple instances problem
-	// /var/lock used to be the place to put this, now it's /run/lock
-	// ...i think
+	/* create a lock file to solve mutliple instances problem /var/lock used to
+	be the place to put this, now it's /run/lock ...i think */
 	struct stat statbuf;
 	int lock_file;
 
-	// try /run/lock first, since i believe it's preferred
+	/* try /run/lock first, since i believe it's preferred */
 	if (!stat("/run/lock", &statbuf))
 	{
 		lock_file = open("/run/lock/" APPNAME ".lock", O_CREAT | O_RDWR, 0666);
@@ -107,12 +111,14 @@ int main(int argc, char **argv)
 	if (rc)
 	{
 		if (EWOULDBLOCK == errno)
+		{
 			die(APPNAME " already running\n");
+		}
 	}
 
 	unsigned int cfg_passwd_timeout;
 
-	// Read user's current theme
+	/* Read user's current theme */
 	m_config_slimlock.readConf(CFGFILE);
 	m_config_slimlock.readConf(SLIMLOCKCFG);
 	std::string themebase = "";
@@ -168,16 +174,14 @@ int main(int argc, char **argv)
 	}
 
 	scr = DefaultScreen(dpy);
-
 	XSetWindowAttributes wa;
 	wa.override_redirect = 1;
 
-	// Create a full screen window
+	/* Create a full screen window */
 	root = RootWindow(dpy, scr);
 	win = XCreateWindow(dpy, root, 0, 0, DisplayWidth(dpy, scr), DisplayHeight(dpy, scr), 0, DefaultDepth(dpy, scr),
 						CopyFromParent, DefaultVisual(dpy, scr), CWOverrideRedirect, &wa);
 	XMapWindow(dpy, win);
-
 	XFlush(dpy);
 
 	for (int len = 1000; len; len--)
@@ -192,20 +196,18 @@ int main(int argc, char **argv)
 
 	XSelectInput(dpy, win, ExposureMask | KeyPressMask);
 
-	// This hides the cursor if the user has that option enabled in their
-	// configuration
+	/* This hides the cursor if the user has that option enabled in their configuration */
 	HideCursor();
 	loginPanel = new Panel(dpy, scr, win, m_config_slimlock, themedir, Panel::Mode_Lock);
-
 	int ret = pam_start(APPNAME, loginPanel->GetName().c_str(), &conv, &pam_handle);
-	// If we can't start PAM, just exit because slimlock won't work right
 
+	/* If we can't start PAM, just exit because slimlock won't work right */
 	if (ret != PAM_SUCCESS)
 	{
 		die("PAM: %s\n", pam_strerror(pam_handle, ret));
 	}
 
-	// disable tty switching
+	/* disable tty switching */
 	if (m_config_slimlock.getOption("tty_lock") == "1")
 	{
 		if ((term = open("/dev/console", O_RDWR)) == -1)
@@ -219,7 +221,7 @@ int main(int argc, char **argv)
 		}
 	}
 
-	// Set up DPMS
+	/* Set up DPMS */
 	unsigned int cfg_dpms_standby, cfg_dpms_off;
 	cfg_dpms_standby = Cfg::string2int(m_config_slimlock.getOption("dpms_standby_timeout").c_str());
 	cfg_dpms_off = Cfg::string2int(m_config_slimlock.getOption("dpms_off_timeout").c_str());
@@ -237,21 +239,21 @@ int main(int argc, char **argv)
 		}
 	}
 
-	// Get password timeout
+	/* Get password timeout */
 	cfg_passwd_timeout = Cfg::string2int(m_config_slimlock.getOption("wrong_passwd_timeout").c_str());
 
-	// Let's just make sure it has a sane value
+	/* Let's just make sure it has a sane value */
 	cfg_passwd_timeout = cfg_passwd_timeout > 60 ? 60 : cfg_passwd_timeout;
 
 	pthread_t raise_thread;
 	pthread_create(&raise_thread, nullptr, RaiseWindow, nullptr);
 
-	// Main loop
+	/* Main loop */
 	while (true)
 	{
 		loginPanel->ResetPasswd();
 
-		// AuthenticateUser returns true if authenticated
+		/* AuthenticateUser returns true if authenticated */
 		if (AuthenticateUser())
 		{
 			break;
@@ -260,19 +262,18 @@ int main(int argc, char **argv)
 		loginPanel->WrongPassword(cfg_passwd_timeout);
 	}
 
-	// kill thread before destroying the window that it's supposed to be
-	// raising
+	/* kill thread before destroying the window that it's supposed to be raising */
 	pthread_cancel(raise_thread);
 	loginPanel->ClosePanel();
 
 	delete loginPanel;
 
-	// Get DPMS stuff back to normal
+	/* Get DPMS stuff back to normal */
 	if (using_dpms)
 	{
 		DPMSSetTimeouts(dpy, dpms_standby, dpms_suspend, dpms_off);
 
-		// turn off DPMS if it was off when we entered
+		/* turn off DPMS if it was off when we entered */
 		if (!dpms_state)
 		{
 			DPMSDisable(dpy);
@@ -319,7 +320,7 @@ static int ConvCallback(int num_msgs, const struct pam_message **msg, struct pam
 {
 	loginPanel->EventHandler(Panel::Get_Passwd);
 
-	// PAM expects an array of responses, one for each message
+	/* PAM expects an array of responses, one for each message */
 	if (num_msgs == 0 || (*resp = (pam_response *)calloc(num_msgs, sizeof(struct pam_message))) == nullptr)
 	{
 		return PAM_BUF_ERR;
@@ -332,7 +333,7 @@ static int ConvCallback(int num_msgs, const struct pam_message **msg, struct pam
 			continue;
 		}
 
-		// return code is currently not used but should be set to zero
+		/* return code is currently not used but should be set to zero */
 		resp[i]->resp_retcode = 0;
 
 		if ((resp[i]->resp = strdup(loginPanel->GetPasswd().c_str())) == nullptr)
@@ -350,7 +351,7 @@ bool AuthenticateUser() { return (pam_authenticate(pam_handle, 0) == PAM_SUCCESS
 
 std::string findValidRandomTheme(const std::string &set)
 {
-	// extract random theme from theme set; return empty string on error
+	/* extract random theme from theme set; return empty string on error */
 	std::string name = set;
 	struct stat buf;
 
@@ -367,7 +368,6 @@ std::string findValidRandomTheme(const std::string &set)
 	do
 	{
 		int sel = Util::random() % themes.size();
-
 		name = Cfg::Trim(themes[sel]);
 		themefile = m_config_slimlock.getOption("themes_dir") + "/" + name + THEMESFILE;
 
@@ -386,12 +386,12 @@ std::string findValidRandomTheme(const std::string &set)
 
 void HandleSignal(int sig)
 {
-	// Get DPMS stuff back to normal
+	/* Get DPMS stuff back to normal */
 	if (using_dpms)
 	{
 		DPMSSetTimeouts(dpy, dpms_standby, dpms_suspend, dpms_off);
 
-		// turn off DPMS if it was off when we entered
+		/* turn off DPMS if it was off when we entered */
 		if (!dpms_state)
 		{
 			DPMSDisable(dpy);
@@ -411,8 +411,7 @@ void HandleSignal(int sig)
 	die(APPNAME ": Caught signal; dying\n");
 }
 
-// i think this should be in an event loop instead of this threaded
-// thing
+/** i think this should be in an event loop instead of this threaded thing */
 void *RaiseWindow(void *data)
 {
 	while (1)
