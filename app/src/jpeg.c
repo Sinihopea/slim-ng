bef8871ed145619193d7d251eeee767f8c9b8825
Antti Aspinen <antti.aspinen@outlook.com>
2026-04-13 23:40:45 +0300

    Move iterator i inside the for-loop

--- app/src/jpeg.c
+++ app/src/jpeg.c
@@ -31,13 +31,14 @@ int read_jpeg(const char *filename, int *width, int *height, unsigned char **rgb
 	struct jpeg_decompress_struct cinfo;
 	struct jpeg_error_mgr jerr;
 	unsigned char *ptr = NULL;
-	unsigned int i;
+	// unsigned int i;
 	unsigned int ipos;
 	FILE *infile = fopen(filename, "rb");
 
 	if (infile == NULL)
 	{
 		fprintf(stderr, "Can not fopen file: %s\n", filename);
+
 		return ret;
 	}
 
@@ -51,6 +52,7 @@ int read_jpeg(const char *filename, int *width, int *height, unsigned char **rgb
 	if (cinfo.output_width >= MAX_DIMENSION || cinfo.output_height >= MAX_DIMENSION)
 	{
 		fprintf(stderr, "Unreasonable dimension found in file: %s\n", filename);
+
 		goto close_file;
 	}
 
@@ -58,15 +60,18 @@ int read_jpeg(const char *filename, int *width, int *height, unsigned char **rgb
 	*height = cinfo.output_height;
 
 	rgb[0] = malloc(3 * cinfo.output_width * cinfo.output_height);
+
 	if (rgb[0] == NULL)
 	{
 		fprintf(stderr, "Can't allocate memory for JPEG file.\n");
+
 		goto close_file;
 	}
 
 	if (cinfo.output_components == 3)
 	{
 		ptr = rgb[0];
+
 		while (cinfo.output_scanline < cinfo.output_height)
 		{
 			jpeg_read_scanlines(&cinfo, &ptr, 1);
@@ -76,18 +81,21 @@ int read_jpeg(const char *filename, int *width, int *height, unsigned char **rgb
 	else if (cinfo.output_components == 1)
 	{
 		ptr = malloc(cinfo.output_width);
+
 		if (ptr == NULL)
 		{
 			fprintf(stderr, "Can't allocate memory for JPEG file.\n");
+
 			goto rgb_free;
 		}
 
 		ipos = 0;
+
 		while (cinfo.output_scanline < cinfo.output_height)
 		{
 			jpeg_read_scanlines(&cinfo, &ptr, 1);
 
-			for (i = 0; i < cinfo.output_width; i++)
+			for (unsigned int i = 0; i < cinfo.output_width; i++)
 			{
 				memset(rgb[0] + ipos, ptr[i], 3);
 				ipos += 3;
